62f36b9db724275bc26e1b06f1f22d3acc26d828
newsundram2018 <newsundram2018@gmail.com>
2022-07-05 16:10:34 +0530

    MDC101 completed

--- mdc_100_series/lib/app.dart
+++ mdc_100_series/lib/app.dart
@@ -24,6 +24,7 @@ class ShrineApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Shrine',
       initialRoute: '/login',
       routes: {
